aab5e4b77e55c4eb2a7ec69c57eabc40cce80992
anchalsaini 1006 <sainideepu361@gmail.com>
2024-04-04 10:27:30 +0530

    React Shopping Cart App

---
+++ src/components/Product.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Product = ({ items, cart, setCart }) => {
+  const addToCart = (id, price, title, description, imgSrc) => {
+    const obj = {
+      id,
+      price,
+      title,
+      description,
+      imgSrc,
+    };
+    setCart([...cart, obj]);
+    console.log("Cart element =", cart);
+
+    toast.warn("Your product is added in Cart", {
+      position: "top-right",
+      autoClose: 1898,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  };
+
+  return (
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={1898}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+
+      <div className="container my-5">
+        <div className="row">
+          {items.map((Product) => {
+            return (
+              <>
+                <div
+                  key={Product.id}
+                  className="col-lg-4 col-md-6 my-3 text-center"
+                >
+                  <div className="card" style={{ width: "18rem" }}>
+                    <Link
+                      to={`/product/${Product.id}`}
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <img
+                        src={Product.imgSrc}
+                        className="card-img-top"
+                        alt="..."
+                      />
+                    </Link>
+                    <div className="card-body">
+                      <h5 className="card-title">{Product.title}</h5>
+                      <p className="card-text">{Product.description}</p>
+                      <button className="btn btn-primary mx-3">
+                        {Product.price} ₹
+                      </button>
+                      <button
+                        onClick={() =>
+                          addToCart(
+                            Product.id,
+                            Product.price,
+                            Product.title,
+                            Product.description,
+                            Product.imgSrc
+                          )
+                        }
+                        className="btn btn-warning"
+                      >
+                        Add To Cart
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Product;
---
+++ src/components/ProductDetails.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { items } from "./Data";
+import Product from "./Product";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const ProductDetails = ({ cart, setCart }) => {
+  const { id } = useParams();
+  const [product, setProduct] = useState({});
+  const [relatedProducts, setRelatedProducts] = useState([]);
+
+  useEffect(() => {
+    const filterProduct = items.filter((product) => product.id == id);
+    setProduct(filterProduct[0]);
+
+    const relatedProducts = items.filter(
+      (anchal) => anchal.category === product.category
+    );
+    setRelatedProducts(relatedProducts);
+  }, [id, product.category]);
+
+  const addToCart = (id, price, title, description, imgSrc) => {
+    const obj = {
+      id,
+      price,
+      title,
+      description,
+      imgSrc,
+    };
+    setCart([...cart, obj]);
+    console.log("Cart element =", cart);
+
+    toast.warn("Your product is added in Cart", {
+      position: "top-right",
+      autoClose: 1898,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  };
+
+  return (
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={1898}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+
+      <div className="container con">
+        <div className="img">
+          <img src={product.imgSrc} alt="" />
+        </div>
+        <div className="text-center">
+          <h1 className="card-title">{product.title}</h1>
+          <p className="card-text">{product.description}</p>
+          <button className="btn btn-primary mx-3">{product.price} ₹</button>
+          <button
+            onClick={() =>
+              addToCart(
+                Product.id,
+                Product.price,
+                Product.title,
+                Product.description,
+                Product.imgSrc
+              )
+            }
+            className="btn btn-warning"
+          >
+            Add To Cart
+          </button>
+        </div>
+      </div>
+      <h1 className="text-center">Related Products</h1>
+      <Product cart={cart} setCart={setCart} items={relatedProducts} />
+    </>
+  );
+};
+
+export default ProductDetails;
